0327fabd3cf9c562285f802ee95cf6158a8de05a
michaelshilovski <mikhail.shilovsky@flexsentlabs.com>
2023-03-28 19:17:58 -0400

    [ADDED] state model

---
+++ app/src/main/java/com/sh/michael/simple_notepad/feature_notes/ui/model/StickyNoteState.kt
@@ -0,0 +1,18 @@
+package com.sh.michael.simple_notepad.feature_notes.ui.model
+
+import androidx.annotation.ColorInt
+import com.sh.michael.simple_notepad.common.interfaces.Identifiable
+import com.sh.michael.simple_notepad.common.model.UiString
+
+data class StickyNoteState(
+    override val id: String,
+
+    @ColorInt
+    val backgroundColor: Int = -1, // change to white default color ?
+    val isRevealed: Boolean = false,
+    val noteText: UiString,
+
+    val onRevealAction: (() -> Unit)? = null,
+    val onPrimaryAction: (() -> Unit)? = null,
+    val onSecondaryAction: (() -> Unit)? = null
+) : Identifiable
