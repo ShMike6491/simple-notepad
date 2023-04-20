e7fb5a0e94645c59c1593f36faa014bdf41cf7e8
michaelshilovski <mikhail.shilovsky@flexsentlabs.com>
2023-04-20 19:35:45 -0400

    [CHANGED] application initialized update

--- app/src/main/java/com/sh/michael/simple_notepad/app/AndroidApplication.kt
+++ app/src/main/java/com/sh/michael/simple_notepad/app/AndroidApplication.kt
@@ -1,12 +1,22 @@
 package com.sh.michael.simple_notepad.app
 
 import android.app.Application
+import com.sh.michael.simple_notepad.BuildConfig
+import com.sh.michael.simple_notepad.app.di.appModules
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class AndroidApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
 
-//        TODO()
+        startKoin {
+            androidLogger(if (BuildConfig.DEBUG) Level.ERROR else Level.NONE)
+            androidContext(this@AndroidApplication)
+            modules(appModules)
+        }
     }
 }
