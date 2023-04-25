192d37940d343977c7e03f5cd29d5910dd44da93
michaelshilovski <mikhail.shilovsky@flexsentlabs.com>
2023-04-25 18:06:37 -0400

    [CHANGED] notes ui state data update

--- app/src/main/java/com/sh/michael/simple_notepad/feature_notes/ui/model/StickyNoteState.kt
+++ app/src/main/java/com/sh/michael/simple_notepad/feature_notes/ui/model/StickyNoteState.kt
@@ -1,15 +1,27 @@
 package com.sh.michael.simple_notepad.feature_notes.ui.model
 
-import androidx.annotation.ColorInt
-import com.sh.michael.simple_notepad.common.interfaces.Identifiable
+import androidx.annotation.ColorRes
+import com.sh.michael.simple_notepad.R
 import com.sh.michael.simple_notepad.common.model.UiString
+import com.sh.michael.simple_notepad.feature_notes.domain.model.BackgroundColor
+import com.sh.michael.simple_notepad.feature_notes.domain.model.IStickyNote
 
 data class StickyNoteState(
     override val id: String,
 
-    @ColorInt
-    val backgroundColor: Int = -1, // change to white default color ?
-    val noteText: UiString,
+    val note: IStickyNote,
 
     val onPrimaryAction: (() -> Unit)? = null
-) : Identifiable
+) : IStickyNote by note
+
+val StickyNoteState.title: UiString get() = UiString.DynamicString(note.noteText)
+
+@get:ColorRes
+val StickyNoteState.background: Int get() = when (note.backgroundColor) {
+    BackgroundColor.BLUE -> R.color.baby_blue
+    BackgroundColor.PINK -> R.color.red_pink
+    BackgroundColor.VIOLET -> R.color.violet
+    BackgroundColor.GREEN -> R.color.light_green
+    BackgroundColor.ORANGE -> R.color.red_orange
+    else -> R.color.white
+}
