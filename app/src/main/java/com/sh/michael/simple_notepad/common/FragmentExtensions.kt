91d66c169cef41fcda6ef2827f90f44ea8bc800a
michaelshilovski <mikhail.shilovsky@flexsentlabs.com>
2023-05-02 18:33:59 -0400

    [CHANGED] fragment keyboard listener extension update

--- app/src/main/java/com/sh/michael/simple_notepad/common/FragmentExtensions.kt
+++ app/src/main/java/com/sh/michael/simple_notepad/common/FragmentExtensions.kt
@@ -1,9 +1,11 @@
 package com.sh.michael.simple_notepad.common
 
+import android.graphics.Rect
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.viewbinding.ViewBinding
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -13,5 +15,21 @@ fun <T> Fragment.collectLatestLifecycleFlow(flow: Flow<T>, collect: suspend (T) 
         repeatOnLifecycle(Lifecycle.State.STARTED) {
             flow.collectLatest(collect)
         }
+    }
+}
+
+fun Fragment.addKeyboardStateListener(binding: ViewBinding, callback: (isOpen: Boolean) -> Unit) {
+    val rootView = binding.root
+
+    rootView.viewTreeObserver.addOnGlobalLayoutListener {
+        val rect = Rect()
+        rootView.getWindowVisibleDisplayFrame(rect)
+        val screenHeight = rootView.rootView.height
+        val keypadHeight = screenHeight - rect.bottom
+
+        // 0.15 ratio is perhaps enough to determine keypad height.
+        val isStateOpen = keypadHeight > screenHeight * 0.15
+
+        callback.invoke(isStateOpen)
     }
 }
