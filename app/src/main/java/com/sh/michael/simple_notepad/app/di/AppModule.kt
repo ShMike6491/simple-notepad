937719873e7f2a7a96ae70fbd11aad3928e89928
michaelshilovski <mikhail.shilovsky@flexsentlabs.com>
2023-07-25 19:12:14 -0300

    [CHANGED] app module update with pages dependencies

--- app/src/main/java/com/sh/michael/simple_notepad/app/di/AppModule.kt
+++ app/src/main/java/com/sh/michael/simple_notepad/app/di/AppModule.kt
@@ -2,6 +2,7 @@ package com.sh.michael.simple_notepad.app.di
 
 import com.sh.michael.simple_notepad.app.data.AppDatabase
 import com.sh.michael.simple_notepad.feature_notes.di.notesModule
+import com.sh.michael.simple_notepad.feature_pages.di.pagesModule
 import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
 
@@ -9,4 +10,4 @@ val appModule = module {
     single { AppDatabase.getDatabase(androidApplication()) }
 }
 
-val appModules = listOf(appModule, notesModule)
+val appModules = listOf(appModule, notesModule, pagesModule)
