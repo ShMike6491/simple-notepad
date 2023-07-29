c7c99d25514f980404e0a57235339b31dd664b9c
michaelshilovski <mikhail.shilovsky@flexsentlabs.com>
2023-07-29 17:49:49 -0300

    [CHANGED] snackbar extension update with custom state passed

--- app/src/main/java/com/sh/michael/simple_notepad/common/SnackBar.kt
+++ app/src/main/java/com/sh/michael/simple_notepad/common/SnackBar.kt
@@ -3,62 +3,22 @@ package com.sh.michael.simple_notepad.common
 import android.graphics.Color
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.annotation.ColorRes
-import androidx.annotation.DrawableRes
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
 import com.sh.michael.simple_notepad.R
-import com.sh.michael.simple_notepad.common.model.UiString
+import com.sh.michael.simple_notepad.common.model.SnackBarState
 import com.sh.michael.simple_notepad.common.model.hasValue
 
 const val SNACKBAR_TAG = "default snackbar"
 
-fun Fragment.showErrorSnackBar(
-    title: UiString? = null,
-    message: UiString? = null,
-    buttonText: UiString? = null,
-    onActionClick: (() -> Unit)? = null
-) = showCustomSnackBar(R.color.error_red, R.color.white, R.drawable.ic_error, title, message, buttonText, onActionClick)
-
-fun Fragment.showSuccessSnackBar(
-    title: UiString? = null,
-    message: UiString? = null,
-    buttonText: UiString? = null,
-    onActionClick: (() -> Unit)? = null
-) = showCustomSnackBar(R.color.green_success, R.color.white, R.drawable.ic_success, title, message, buttonText, onActionClick)
-
-fun Fragment.showBrandedSnackBar(
-    @DrawableRes icon: Int? = null,
-    title: UiString? = null,
-    message: UiString? = null,
-    buttonText: UiString? = null,
-    onActionClick: (() -> Unit)? = null
-) = showCustomSnackBar(R.color.banana_yellow, R.color.black, icon, title, message, buttonText, onActionClick)
-
-fun Fragment.showDefaultSnackBar(
-    @DrawableRes icon: Int? = null,
-    title: UiString? = null,
-    message: UiString? = null,
-    buttonText: UiString? = null,
-    onActionClick: (() -> Unit)? = null
-) = showCustomSnackBar(R.color.blue_netrual, R.color.white, icon, title, message, buttonText, onActionClick)
-
-fun Fragment.showCustomSnackBar(
-    @ColorRes backgroundColor: Int? = null,
-    @ColorRes contentColor: Int = R.color.white,
-    @DrawableRes icon: Int? = null,
-    title: UiString? = null,
-    message: UiString? = null,
-    buttonText: UiString? = null,
-    onActionClick: (() -> Unit)? = null
-) {
+fun Fragment.showSnackBar(state: SnackBarState) = state.apply {
     val hasIcon = icon != null
     val hasTitle = title.hasValue(context)
     val hasMessage = message.hasValue(context)
     val hasButton = buttonText.hasValue(context)
 
-    val snackbar = Snackbar.make(this.requireView(), SNACKBAR_TAG, Snackbar.LENGTH_LONG)
+    val snackbar = Snackbar.make(this@showSnackBar.requireView(), SNACKBAR_TAG, Snackbar.LENGTH_LONG)
 
     val customSnackView = layoutInflater.inflate(R.layout.item_snackbar, null).apply {
         backgroundColor?.let {
