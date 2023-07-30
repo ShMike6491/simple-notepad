680d8a4b2bde3ed733dfa663a90eedbdca3a8571
michaelshilovski <mikhail.shilovsky@flexsentlabs.com>
2023-07-30 18:57:51 -0300

    [CHANGED] page state changes complete

--- app/src/main/java/com/sh/michael/simple_notepad/feature_pages/ui/model/PageState.kt
+++ app/src/main/java/com/sh/michael/simple_notepad/feature_pages/ui/model/PageState.kt
@@ -1,7 +1,10 @@
 package com.sh.michael.simple_notepad.feature_pages.ui.model
 
+import com.sh.michael.simple_notepad.R
 import com.sh.michael.simple_notepad.common.interfaces.Identifiable
+import com.sh.michael.simple_notepad.common.model.UiError
 import com.sh.michael.simple_notepad.common.model.UiString
+import com.sh.michael.simple_notepad.common.model.UiString.*
 
 data class PageState(
     override val id: String,
@@ -9,7 +12,17 @@ data class PageState(
     val hintText: UiString? = null,
     val pageTag: UiString? = null,
 
+    val error: UiError? = null,
+    val isLoading: Boolean = true,
+    val isPageEnabled: Boolean = false,
     val onTextChangeAction: ((sequence: CharSequence?) -> Unit)? = null
 ) : Identifiable {
-    val bodyText: UiString? get() = valueText?.let { UiString.DynamicString(it) }
+    val bodyText: UiString? get() = valueText?.let { DynamicString(it) }
+    val hasError: Boolean get() = error != null
 }
+
+val pageErrorState = UiError(
+    errorImage = R.drawable.image_error_robot,
+    titleText = StringResource(R.string.something_went_wrong),
+    messageText = StringResource(R.string.page_error_helper)
+)
