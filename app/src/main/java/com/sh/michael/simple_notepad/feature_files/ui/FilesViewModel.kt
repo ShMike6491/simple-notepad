f3fd5fb22920ec569eb73043119af3162d077fa4
michaelshilovski <mikhail.shilovsky@flexsentlabs.com>
2023-08-07 18:31:21 -0300

    [CHANGED] files view model complete

--- app/src/main/java/com/sh/michael/simple_notepad/feature_files/ui/FilesViewModel.kt
+++ app/src/main/java/com/sh/michael/simple_notepad/feature_files/ui/FilesViewModel.kt
@@ -5,13 +5,11 @@ import androidx.lifecycle.viewModelScope
 import com.sh.michael.simple_notepad.R
 import com.sh.michael.simple_notepad.common.model.UiError
 import com.sh.michael.simple_notepad.common.model.UiEvent
-import com.sh.michael.simple_notepad.common.model.UiString
 import com.sh.michael.simple_notepad.common.model.UiString.*
 import com.sh.michael.simple_notepad.feature_files.domain.IFilesRepository
 import com.sh.michael.simple_notepad.feature_files.domain.model.IFile
 import com.sh.michael.simple_notepad.feature_files.ui.model.FileState
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
@@ -31,8 +29,8 @@ class FilesViewModel(
 
     val defaultEmptyState = UiError(
         errorImage = R.drawable.image_question,
-        titleText = DynamicString("Nothing found..."),
-        messageText = DynamicString("Looks like there are no files found in your app.")
+        titleText = StringResource(R.string.nothing_found),
+        messageText = StringResource(R.string.empty_files_helper)
     )
 
     private fun mapToStateData(files: List<IFile>): List<FileState> {
