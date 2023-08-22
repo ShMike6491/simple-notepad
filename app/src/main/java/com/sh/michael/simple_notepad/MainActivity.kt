dfa89184cfefe977e0ebd4869364fefaa3ff0642
michaelshilovski <mikhail.shilovsky@flexsentlabs.com>
2023-08-22 19:46:04 -0300

    [CHANGED] gestures disabled for handle view

--- app/src/main/java/com/sh/michael/simple_notepad/MainActivity.kt
+++ app/src/main/java/com/sh/michael/simple_notepad/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.sh.michael.simple_notepad
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.sh.michael.simple_notepad.common.DrawerHandle
+import com.sh.michael.simple_notepad.common.disableGlobalGestures
 import com.sh.michael.simple_notepad.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +16,19 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // todo: disable swipe gesture for this view
+        DrawerHandle.attach(
+            binding.drawerNavigationView,
+            R.layout.menu_handle,
+            HANDLE_OFFSET
+        )
+
+        findViewById<ImageView>(R.id.handleImageView).disableGlobalGestures()
+    }
+
+    companion object {
+
+        const val HANDLE_OFFSET = 0.7f
     }
 }
