8e8ccb6941c61ec3d468415f5fb1451bfae1887d
michaelshilovski <mikhail.shilovsky@flexsentlabs.com>
2023-08-24 15:13:03 -0300

    [CHANGED] default snackbar color changed to blue

--- app/src/main/java/com/sh/michael/simple_notepad/common/model/SnackBarState.kt
+++ app/src/main/java/com/sh/michael/simple_notepad/common/model/SnackBarState.kt
@@ -7,7 +7,7 @@ import com.sh.michael.simple_notepad.R
 val ERROR_SNACKBAR = SnackBarState(R.color.error_red, R.color.white, R.drawable.ic_error, null, null, null, null)
 val SUCCESS_SNACKBAR = SnackBarState(R.color.green_success, R.color.white, R.drawable.ic_success, null, null, null, null)
 val BRANDED_SNACKBAR = SnackBarState(R.color.banana_yellow, R.color.black, null, null, null, null, null)
-val DEFAULT_SNACKBAR = SnackBarState(R.color.granite_gray, R.color.white, null, null, null, null, null)
+val DEFAULT_SNACKBAR = SnackBarState(R.color.blue_netrual, R.color.white, null, null, null, null, null)
 
 data class SnackBarState(
     @ColorRes
