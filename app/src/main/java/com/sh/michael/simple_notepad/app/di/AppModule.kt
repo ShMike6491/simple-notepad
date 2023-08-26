b01e2e028fb302b10d85108a9502a802251efb2e
michaelshilovski <mikhail.shilovsky@flexsentlabs.com>
2023-08-26 13:21:12 -0300

    [CHANGED] application module update with new  menu di

--- app/src/main/java/com/sh/michael/simple_notepad/app/di/AppModule.kt
+++ app/src/main/java/com/sh/michael/simple_notepad/app/di/AppModule.kt
@@ -3,6 +3,7 @@ package com.sh.michael.simple_notepad.app.di
 import com.sh.michael.simple_notepad.app.data.AppDatabase
 import com.sh.michael.simple_notepad.feature_files.di.filesModule
 import com.sh.michael.simple_notepad.feature_home.di.homeModule
+import com.sh.michael.simple_notepad.feature_menu.di.menuModule
 import com.sh.michael.simple_notepad.feature_notes.di.notesModule
 import com.sh.michael.simple_notepad.feature_pages.di.pagesModule
 import org.koin.android.ext.koin.androidApplication
@@ -12,4 +13,4 @@ val appModule = module {
     single { AppDatabase.getDatabase(androidApplication()) }
 }
 
-val appModules = listOf(appModule, notesModule, pagesModule, filesModule, homeModule)
+val appModules = listOf(appModule, notesModule, pagesModule, filesModule, homeModule, menuModule)
